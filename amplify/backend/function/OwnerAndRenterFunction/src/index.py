b45d544495f2dd6bd13c371c40086740ad785114
Efrr2022 <efrsul8@gmail.com>
2024-05-04 12:17:17 +0200

    logger added

--- amplify/backend/function/OwnerAndRenterFunction/src/index.py
+++ amplify/backend/function/OwnerAndRenterFunction/src/index.py
@@ -2,9 +2,9 @@ import json
 import mysql.connector 
 import config
 from botocore.exceptions import ClientError
-from time import strftime
 import re
 import logging
+import datetime
 
 
 
@@ -19,10 +19,6 @@ def handler(event, context):
     # Create handlers
     c_handler = logging.StreamHandler()
     c_handler.setLevel(logging.INFO)
-    
-    # Create formatters and add it to handlers
-    c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
-    c_handler.setFormatter(c_format)
 
     # Add handlers to the logger
     logger.addHandler(c_handler)
