36a1883f63178461da64d91d23cd53fe0badece8
Efrr2022 <efrsul8@gmail.com>
2024-05-12 22:43:40 +0200

    auth added

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { Amplify } from 'aws-amplify';
-import aws_exports from './aws-exports';
-Amplify.configure(aws_exports);
+import amplifyconfig from './amplifyconfiguration.json';
+Amplify.configure(amplifyconfig);
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
