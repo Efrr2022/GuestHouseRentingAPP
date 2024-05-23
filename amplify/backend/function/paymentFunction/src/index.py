8c50153c27bb8110a8856b723b55a27ec7b584b3
natnael melake <natnaelmelake42@gmail.com>
2024-05-23 19:08:26 +0300

    modified payment function

--- amplify/backend/function/paymentFunction/src/index.py
+++ amplify/backend/function/paymentFunction/src/index.py
@@ -117,8 +117,7 @@ def handle_get_payment(event,db):
 
             response_get={
                 'statusCode': 200,
-                'body': json.dumps(response_list, default=str)  # Serialize datetime objects using default=str
-
+                'body': response_list # Serialize datetime objects using default=str
             }
 
             return response_get
